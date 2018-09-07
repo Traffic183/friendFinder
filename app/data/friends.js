c92da9422b881631a2fab0b5aa80ba656e5b65c0
Mara <levymaratx@gmail.com>
2018-09-06 23:02:03 -0500

    first pass

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+let friends = [
+  {
+    name: "Sara Sharkey",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Ellie Gould",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Evans Peters",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Georgia Python",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Sam Ritter",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Alpha Bits",
+    photo: "https://via.placeholder.com/200x200.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+module.exports = friends;
